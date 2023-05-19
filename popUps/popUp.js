284cda85ce4a45f2677e1ad05b58bdf47d7c2d75
Emmanuel Ekeleme <51166118+emyzyk@users.noreply.github.com>
2023-05-19 19:25:00 +0100

    annoying browser pop ups

---
+++ popUps/popUp.js
@@ -0,0 +1,19 @@
+
+// ---------------- HOW TO CREATE A POPUPS -----------------------
+
+const button = document.querySelector('button');
+const popup = document.querySelector('.popup-wrapper')
+const close = document.querySelector('.popup-close');
+
+button.addEventListener('click', () => {
+    popup.style.display = 'block';
+});
+
+close.addEventListener('click', () => {
+    popup.style.display = 'none';
+});
+
+popup.addEventListener('click', () => {
+    popup.style.display = 'none';
+});
+
